33a40327781fb58196f5f4e2cf20de0531c6b17b
Riddhesh Shah <riddhesh.shah@mail.utoronto.ca>
2022-11-27 20:32:48 -0500

    Added sbert and phrase bert setup dependencies

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ source summ-env/bin/activate
 #env/Scripts/Activate.ps1 //In Powershel
 pip install -r requirements.txt
 python download_nltk_pakages.py
+wget https://storage.googleapis.com/phrase-bert/phrase-bert/phrase-bert-model.zip
+unzip phrase-bert-model.zip -d phrase-bert-model/
+rm phrase-bert-model.zip
